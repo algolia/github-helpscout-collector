1a4295f046054f32c61cf601d80c7f157ba8684a
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-12-30 19:25:38 +0100

    feat: validate event type

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,39 @@
-describe('index', () => {
-  it('expect to pass', () => {
-    expect(true).toBe(true);
+import micro from 'micro';
+import listen from 'test-listen';
+import axios from 'axios';
+import run from '../index';
+
+describe('App', () => {
+  it('expect to throw an error without the `X-GitHub-Event` header', async () => {
+    expect.assertions(1);
+
+    const endpoint = await listen(micro(run));
+
+    try {
+      await axios.post(endpoint, null, {
+        headers: {
+          'X-Github-Delivery': 'XXXX-XXXX-XXXX-XXXX',
+        },
+      });
+    } catch (error) {
+      expect(error.response.status).toBe(400);
+    }
+  });
+
+  it('expect to throw an error with the `X-GitHub-Event` header different than `issues`', async () => {
+    expect.assertions(1);
+
+    const endpoint = await listen(micro(run));
+
+    try {
+      await axios.post(endpoint, null, {
+        headers: {
+          'X-Github-Delivery': 'XXXX-XXXX-XXXX-XXXX',
+          'X-GitHub-Event': 'membership',
+        },
+      });
+    } catch (error) {
+      expect(error.response.status).toBe(400);
+    }
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import { RequestHandler } from 'micro';
+import { RequestHandler, createError, send } from 'micro';
 
-const run: RequestHandler = () => {
-  return 'Hello World';
+const run: RequestHandler = async (req, res) => {
+  const githubEventTypeHeader = req.headers['x-github-event'];
+
+  // Only support issues event
+  if (!githubEventTypeHeader || githubEventTypeHeader !== 'issues') {
+    throw createError(400, 'Request not supported');
+  }
+
+  send(res, 201);
 };
 
 export default run;
