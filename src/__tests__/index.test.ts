9f48da856929a41e6a81031926a14509a38abe6c
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-12-30 19:37:48 +0100

    feat: validation action type

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -5,7 +5,7 @@ import run from '../index';
 
 describe('App', () => {
   it('expect to throw an error without the `X-GitHub-Event` header', async () => {
-    expect.assertions(1);
+    expect.assertions(2);
 
     const endpoint = await listen(micro(run));
 
@@ -17,11 +17,14 @@ describe('App', () => {
       });
     } catch (error) {
       expect(error.response.status).toBe(400);
+      expect(error.response.data).toMatchInlineSnapshot(
+        `"Only the event \`issues\` is supported by the hook"`
+      );
     }
   });
 
   it('expect to throw an error with the `X-GitHub-Event` header different than `issues`', async () => {
-    expect.assertions(1);
+    expect.assertions(2);
 
     const endpoint = await listen(micro(run));
 
@@ -34,6 +37,55 @@ describe('App', () => {
       });
     } catch (error) {
       expect(error.response.status).toBe(400);
+      expect(error.response.data).toMatchInlineSnapshot(
+        `"Only the event \`issues\` is supported by the hook"`
+      );
+    }
+  });
+
+  it('expect to throw an error with an `action` different than `created`', async () => {
+    expect.assertions(4);
+
+    const endpoint = await listen(micro(run));
+
+    try {
+      await axios.post(
+        endpoint,
+        {
+          action: 'deleted',
+        },
+        {
+          headers: {
+            'X-Github-Delivery': 'XXXX-XXXX-XXXX-XXXX',
+            'X-GitHub-Event': 'issues',
+          },
+        }
+      );
+    } catch (error) {
+      expect(error.response.status).toBe(400);
+      expect(error.response.data).toMatchInlineSnapshot(
+        `"Only the action \`created\` is supported by the hook"`
+      );
+    }
+
+    try {
+      await axios.post(
+        endpoint,
+        {
+          action: 'edited',
+        },
+        {
+          headers: {
+            'X-Github-Delivery': 'XXXX-XXXX-XXXX-XXXX',
+            'X-GitHub-Event': 'issues',
+          },
+        }
+      );
+    } catch (error) {
+      expect(error.response.status).toBe(400);
+      expect(error.response.data).toMatchInlineSnapshot(
+        `"Only the action \`created\` is supported by the hook"`
+      );
     }
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-import { RequestHandler, createError, send } from 'micro';
+import { RequestHandler, createError, json, send } from 'micro';
+
+type IssueEventPayload = {
+  action: string;
+};
 
 const run: RequestHandler = async (req, res) => {
   const githubEventTypeHeader = req.headers['x-github-event'];
 
-  // Only support issues event
+  // Only support `issues` event
   if (!githubEventTypeHeader || githubEventTypeHeader !== 'issues') {
-    throw createError(400, 'Request not supported');
+    throw createError(400, 'Only the event `issues` is supported by the hook');
+  }
+
+  const body = (await json(req)) as IssueEventPayload;
+
+  // Only support `created` action
+  if (body.action !== 'created') {
+    throw createError(400, 'Only the action `created` is supported by the hook');
   }
 
   send(res, 201);
