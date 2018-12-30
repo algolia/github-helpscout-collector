03cfbc44754295f3cf9410831d7f265adef30c3c
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-12-30 14:06:06 +0100

    chore: Jest add TypeAhead

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 };
