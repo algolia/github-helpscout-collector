784136bea88c2cb42a1f3084460e38eef931c087
Vaillant Samuel <samuel.vllnt@gmail.com>
2019-09-27 16:51:39 +0200

    refactor(service): create a main to launch the service

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { createHelpScoutClient } from './helpScoutClient';
+import { createService } from './createService';
+
+const githubWebhookSecret = process.env.GITHUB_WEBHOOK_SECRET || 'mySuperSecretToken';
+
+const helpScoutAppId = process.env.HELP_SCOUT_APP_ID || '';
+const helpScoutAppSecret = process.env.HELP_SCOUT_APP_SECRET || '';
+
+const helpScoutMailboxes = (process.env.HELP_SCOUT_MAILBOXES &&
+  JSON.parse(process.env.HELP_SCOUT_MAILBOXES)) || {
+  data: [],
+};
+
+const helpScoutClient = createHelpScoutClient({
+  appId: helpScoutAppId,
+  appSecret: helpScoutAppSecret,
+});
+
+export default createService({
+  githubWebhookSecret,
+  helpScoutClient,
+  helpScoutMailboxes,
+});
