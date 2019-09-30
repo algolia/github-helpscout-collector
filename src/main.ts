407e628a7bc8c99a0cb4a7fb92f328ff9cd8e714
Vaillant Samuel <samuel.vllnt@gmail.com>
2019-09-30 09:54:30 +0200

    fix(main): correctly parse the JSON value

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ const helpScoutAppId = process.env.HELP_SCOUT_APP_ID || '';
 const helpScoutAppSecret = process.env.HELP_SCOUT_APP_SECRET || '';
 
 const helpScoutMailboxes = (process.env.HELP_SCOUT_MAILBOXES &&
-  JSON.parse(process.env.HELP_SCOUT_MAILBOXES)) || {
+  JSON.parse(JSON.stringify(process.env.HELP_SCOUT_MAILBOXES))) || {
   data: [],
 };
 
